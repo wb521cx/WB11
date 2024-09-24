d0bfbc50c40dc42a107aaacdaf17fc19bd6e6a1d
wb521cx <145813019+wb521cx@users.noreply.github.com>
2024-09-24 14:12:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname = 'remote.xmimx.xyz'
+        url.hostname = 'c.wb1973.vip'
         let new_request = new Request(url, request);
         return fetch(new_request);
       }
